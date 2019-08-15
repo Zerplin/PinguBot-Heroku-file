7183b372555e5facea68f509dd35a3a0aba9489f
Zerplin <43621940+Zerplin@users.noreply.github.com>
2019-08-15 16:14:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,9 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 require('dotenv').config();
 
+const DBL = require("dblapi.js");
+const dbl = new DBL('process.env.token', bot);
+
 var msg;
 var msgL;
 var oL=2;
